5149f4c57c35191823359588274ff5d2621467d1
raphaelsurmacz <raphael.surmacz@gmail.com>
2025-03-07 10:13:59 -0300

    try to create a header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css'
+import './styles/App.css'
 import Header from './components/Header';
 import Home from './pages/Home';
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import logo from '../assets/react.svg'
+import '../styles/Header.css'
 
 const Header = () => {
     return (
@@ -9,9 +10,23 @@ const Header = () => {
                 <img src={logo} alt="Logo" className="colormate__logo" />
             </Link>
 
+            <nav className='header__nav'>
+                <Link to='/'>
+                    Home
+                </Link>
+
+                <Link to='/'>
+                    About
+                </Link>
+                
+                <button>Logout</button>
+            </nav>
+
+            
+            {/* 
             <Link to='/' className='header__link'>
                 <h1>Color Mate</h1>
-            </Link>
+            </Link> */}
         </div>
     )
 };
