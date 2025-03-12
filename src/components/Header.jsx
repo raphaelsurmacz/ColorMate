8c930d3f6c7be61b8348de641a0db855e68ad3bf
raphaelsurmacz <raphael.surmacz@gmail.com>
2025-03-12 11:43:06 -0300

    header almost ready

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,28 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../assets/react.svg'
-import '../styles/Header.css'
+import logo from '../assets/react.svg';
+import '../styles/Header.css';
 
 const Header = () => {
     return (
-        <div className="header">
+        <header className="header">
             <Link to='/'>
                 <img src={logo} alt="Logo" className="colormate__logo" />
             </Link>
 
             <nav className='header__nav'>
-                <Link to='/'>
-                    Home
-                </Link>
-
-                <Link to='/'>
-                    About
-                </Link>
-                
-                <button>Logout</button>
+                <ul className='header__nav-list'>
+                    <li>
+                        <Link to='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/about'>About</Link>
+                    </li>
+                </ul>
+                <button className="header__logout">Logout</button>
             </nav>
-
-            
-            {/* 
-            <Link to='/' className='header__link'>
-                <h1>Color Mate</h1>
-            </Link> */}
-        </div>
-    )
+        </header>
+    );
 };
 
 export default Header;
